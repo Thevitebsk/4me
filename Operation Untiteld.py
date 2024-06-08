991896800ef49c1ee6a51d5e74fff360e9e0c0ba
Gaham <155390134+Thevitebsk@users.noreply.github.com>
2024-06-08 21:48:23 +0300

    Add files via upload

---
+++ Operation Untiteld.py
@@ -0,0 +1,9 @@
+cmd = "Untiteld:"
+while True:
+    comm = input(cmd)
+    if comm == "info":
+        print('Operation "Untiteld"')
+        print("Version 0.1")
+        print("Created by Thevitebsk/Gaham")
+    else:
+        print("?")
