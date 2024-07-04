2c019f09c3a2616e95bb772779be3bb54f6d3086
Gaham <155390134+Thevitebsk@users.noreply.github.com>
2024-07-04 19:57:36 +0300

    Update Operation Untiteld.py

--- Operation Untiteld.py
+++ Operation Untiteld.py
@@ -1,27 +1,26 @@
-import random
-text = []
-cmd = "Untiteld:"
-rollnum = ["1","2","3","4","5","6","7","8","9","a","b","c","d","e","f","g","h","i"]
+from tkinter import *
+from tkinter.filedialog import askopenfilename
+def imp():
+    c=0
+    pa= askopenfilename(filetypes=[("4ME Files", "*.fme")])
+    while c==0:
+        try:
+            with open(pa, 'r') as file:
+                global con
+                con = file.read()
+                print(con)
+                c=1
+        except FileNotFoundError:
+            print("Unknown file")
+            c=0
 while True:
-    comm = input(cmd)
-    if comm == "info":
-        print('Operation "Untiteld"')
-        print("Version 0.1")
-        print("Created by Thevitebsk/Gaham")
-    elif comm == "txt":
-        print("Enter your text:\n")
-        while True:
-            txt = input()
-            if txt.lower() == "save":
-                break
-            text.append(txt)
-        with open("CREATED TROUGH OPERATION UNTITELD.txt", "w") as file:
-            for out in text[:-1]:
-                file.write(out + "\n")
-            if text:
-                file.write(text[-1] + "\nCREATED TROUGH OPERATION UNTITELD")
-    elif comm == "roll":
-        roll = random.choice(rollnum)
-        print(roll)
-    else:
-        print("?")
+    inp=0
+    i=imp()
+    print(i)
+    if i in "out":
+        i=i-"out"
+        print(i)
+    if i=="put INPUT to work-machine":
+        inp=1
+    if i=="E":
+        break
