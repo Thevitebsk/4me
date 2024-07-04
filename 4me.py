cd5492669726033fdb61fb2f1725bda59ed6277c
Gaham <155390134+Thevitebsk@users.noreply.github.com>
2024-07-04 22:27:12 +0300

    Update 4me.py

--- 4me.py
+++ 4me.py
@@ -10,7 +10,7 @@ def imp():
                 lines = [line.rstrip() for line in file]
             return lines
         except FileNotFoundError:
-            print("S?\nHOW DID YOU GET IN HERE?")
+            print()
 #main code loop
 while True:
     error=False
@@ -18,16 +18,15 @@ while True:
     inpu = ""
     i = imp()
     ln=1
+    sm=0
     if i is not None:
         ln+=1
         for line in i:
-            matches = re.findall(r'{(.*?)}', line)
+            matches = re.findall(r'out{(.*?)}', line)
             for match in matches:
                 print(match)
             if line == "put INPUT to work-machine":
                 inp = 1
-            if line == "show stack":
-                print(s)
             if line == "in{ }":
                 if inp==1:
                     inpu=input("string:")
@@ -37,5 +36,23 @@ while True:
                     error=True
             if line == "":
                 print(f"S1456\nCAN YOU. IDK. USE COMMANDS IN THE LINE\nERROR AT LINE {ln}")
+            if line == "put STACK MANIPULATION to work-machine":
+                sm = 1
+            matches = re.findall(r'add{(.*?)}', line)
+            for match in matches:
+                if sm==1:
+                    s.append(match)
+                if sm==0:
+                    print(f"S6732\nMODULE STACK MANIPULATION IS NOT FOUND\nERROR AT LINE {ln}")
+            if line == "remove":
+                if sm==1:
+                    s.pop(matches)
+                if sm==0:
+                    print(f"S6732\nMODULE STACK MANIPULATION IS NOT FOUND\nERROR AT LINE {ln}")
+            if line == "get":
+                if sm==1:
+                    print(s[0])
+                if sm==0:
+                    print(f"S6732\nMODULE STACK MANIPULATION IS NOT FOUND\nERROR AT LINE {ln}")
             if error==True:
                 break
