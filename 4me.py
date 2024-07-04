1cc62ac3116fd1a383e1095b9ff1e8c832f1f877
Gaham <155390134+Thevitebsk@users.noreply.github.com>
2024-07-04 21:53:31 +0300

    more updating horray

--- 4me.py
+++ 4me.py
@@ -2,7 +2,6 @@
 s=[]
 from tkinter.filedialog import askopenfilename
 import re
-#import file
 def imp():
     pa = askopenfilename(filetypes=[("4ME Files", "*.fme")])
     if pa:
@@ -14,16 +13,29 @@ def imp():
             print()
 #main code loop
 while True:
+    error=False
     inp = 0
+    inpu = ""
     i = imp()
+    ln=1
     if i is not None:
+        ln+=1
         for line in i:
             matches = re.findall(r'{(.*?)}', line)
             for match in matches:
                 print(match)
             if line == "put INPUT to work-machine":
                 inp = 1
-            if line == "end execution":
-                break
             if line == "show stack":
                 print(s)
+            if line == "in{ }":
+                if inp==1:
+                    inpu=input("string:")
+                    s.append(inpu)
+                if inp==0:
+                    print(f"S6732\nMODULE INPUT IS NOT FOUND\nERROR AT LINE {ln}")
+                    error=True
+            if line == "":
+                print(f"S1456\nCAN YOU. IDK. USE COMMANDS IN THE LINE\nERROR AT LINE {ln}")
+            if error==True:
+                break
