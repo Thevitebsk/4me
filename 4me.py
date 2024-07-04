8f37a98b9d72523db39bbffa357266350c494ffb
Gaham <155390134+Thevitebsk@users.noreply.github.com>
2024-07-04 20:03:54 +0300

    more shortage? i think

--- 4me.py
+++ 4me.py
@@ -1,18 +1,16 @@
 #THIS IMPLEMENTATION OF 4ME IS BUGGY
 from tkinter.filedialog import askopenfilename
 def imp():
-    c=0
-    pa= askopenfilename(filetypes=[("4ME Files", "*.fme")])
-    while c==0:
+    pa=askopenfilename(filetypes=[("4ME Files", "*.fme")])
+    if pa:
         try:
             with open(pa, 'r') as file:
                 global con
                 con = file.read()
                 print(con)
-                c=1
         except FileNotFoundError:
             print("Unknown file")
-            c=0
+            break
 while True:
     inp=0
     i=imp()
