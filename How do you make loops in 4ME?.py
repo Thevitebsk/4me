b5398f8e5e9608f77d1c08d6f3772136a0a95431
Gaham <155390134+Thevitebsk@users.noreply.github.com>
2024-07-07 13:48:45 +0300

    Update How do you make loops in 4ME?.py

--- How do you make loops in 4ME?.py
+++ How do you make loops in 4ME?.py
@@ -1,4 +1,4 @@
-#this is a showcase of how you do loops in 4ME implemented trough python since 4ME doesn't have loops
+#this is a showcase of how you do loops in 4ME implemented trough python since 4ME doesn't have loops and conditions
 def func():
     print("hi")
     if True:
