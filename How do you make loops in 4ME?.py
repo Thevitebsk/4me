ec4b7ddf0071ed5946838719d96136ede3f59104
Gaham <155390134+Thevitebsk@users.noreply.github.com>
2024-09-23 00:01:21 +0300

    Update How do you make loops in 4ME?.py

--- How do you make loops in 4ME?.py
+++ How do you make loops in 4ME?.py
@@ -1,6 +1,1 @@
-#this is a showcase of how you do loops in 4ME implemented trough python since 4ME doesn't have loops and conditions
-def func():
-    print("hi")
-    if True:
-        func()
-func()
+print("impossible")
