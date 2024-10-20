5fb66ee01e5c04b0f1783ebca208bcb58342ff72
Gaham <155390134+Thevitebsk@users.noreply.github.com>
2024-10-20 21:26:17 +0300

    Update fme.py

--- Rewritten 4me/fme.py
+++ Rewritten 4me/fme.py
@@ -1,4 +1,4 @@
 import os
 print("{R4ME}")
 f=input("Input a filename that is inside the directory:")
-dir=os.path.realpath(__file__);dir2=os.path.dirname(dir);o=open(f"{dir2}\\{f}.vap");f=o.read()
+dir=os.path.realpath(__file__);dir2=os.path.dirname(dir);o=open(f"{dir2}\\{f}.4");f=o.read()
