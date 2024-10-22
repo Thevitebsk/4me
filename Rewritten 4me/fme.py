d3df526c7f64cbe03b96b98e49471f0e55df372e
Gaham <155390134+Thevitebsk@users.noreply.github.com>
2024-10-22 11:24:23 +0300

    holy hell there is now arguments

--- Rewritten 4me/fme.py
+++ Rewritten 4me/fme.py
@@ -1,10 +1,14 @@
 import os
 print("{R4ME}")
-var={"values":[],"names":[]}
-f=input("Input a filename that is inside the directory:");p=0;s=[];val=None
-dir=os.path.realpath(__file__);dir2=os.path.dirname(dir);o=open(f"{dir2}\\{f}.4");j=o.read();o=open(f"{dir2}\\{f}.4");l=o.readlines()
+var={"values":[],"names":[]};a={"args":[]}
+f=input("Input a filename that is inside the directory\n");p=0;s=[];val=None;print("\nOutput:")
+dir=os.path.realpath(__file__);dir2=os.path.dirname(dir)
+if "s" in a["args"]:
+    o=open(f"{f}.4")
+else:
+    o=open(f"{dir2}\\{f}.4")
+l=o.readlines()
 l.append("\n");l.reverse();a=str(l[1])+str(l[0]);l.pop(0);l.pop(0);l.reverse();l.append(a)
-print("bytes",len(j),"lines",len(l))
 while l!=[]:
     s=l[0].split("{")
     if s[0]=="echo":print(s[1].replace("}\n",""))
