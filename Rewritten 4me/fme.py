5c45f5d4ff92ab0001bb38b5d8640c255e89d7d8
Gaham <glebsemenov1mail.ru@gmail.com>
2024-12-08 23:03:18 +0300

    did some slight modifacations

--- Rewritten 4me/fme.py
+++ Rewritten 4me/fme.py
@@ -1,17 +1,17 @@
 import os
 print("{R4ME}")
-var={"values":[],"names":[]};a={"args":[]}
-f=input("Input a filename inside the interpriters directory:");p=0;s=[];val=0;print("\nOutput:")
-dir=os.path.realpath(__file__);dir2=os.path.dirname(dir)
-if "s" in a["args"]:o=open(f"{f}.4")
+var={"values":[],"names":[]}
+a={"args":[]}
+f=input("Input a filename inside the interpriters directory:");p=0;s=[];val=0;dir=os.path.realpath(__file__);dir2=os.path.dirname(dir)
+print("\nOutput:")
+if "s"in a["args"]:o=open(f"{f}.4")
 else:o=open(f"{dir2}\\{f}.4")
 l=o.readlines();l.append("\n");l.append(str(l.pop(len(l)-2))+str(l.pop()))
 while l!=[]:
     s=l[0].split("{")
     if s[0]=="echo":print(s[1].replace("}\n",""))
     elif s[0]=="var":
-        s.pop(0)
-        f=s[0].split("=")
+        s.pop(0);f=s[0].split("=")
         if f[1]=="user}\n":var["values"].append(input())
         else:var["values"].append(f[1].replace("\n",""))
         var["names"].append(f[0])
