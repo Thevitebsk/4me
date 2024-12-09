4a55eaae06c34f9239dab13200caddb57d56ff1e
Gaham <glebsemenov1mail.ru@gmail.com>
2024-12-10 00:14:40 +0300

    Update 4me.py

--- 4me.py
+++ 4me.py
@@ -6,41 +6,26 @@ def imp():
     pa = askopenfilename(filetypes=[("Work-Machine Files", "*.wm")])
     if pa:
         try:
-            with open(pa, 'r') as file:lines = [line.rstrip() for line in file]
+            with open(pa, 'r') as file: lines = [line.rstrip() for line in file]
             return lines
-        except FileNotFoundError:
-            print("undefined file")
+        except FileNotFoundError: print("undefined file")
 # main code loop
 while True:
     error = False;inpu = "";i = imp();ln = 1;s=[];ss=[];ts=[];v="";c=0
     if i != None:
         for line in i:
             ln += 1
-            if line=="P;":
-                c=1
+            if line=="P;":c=1
             if c==1:
                 matches = re.findall(r'out{(.*?)}', line)
-                for match in matches:
-                    print(match)
-                if line == "is":
-                    inpu = input("string:")
-                    s.append(inpu)
-                if line == "ns":
-                    inpu = input("number:")
-                    s.append(int(inpu))
-                if line == "":
-                    print(f"S1456\nCAN YOU. I DON'T KNOW. USE COMMANDS IN THE LINE\nERROR AT LINE {ln}")
-                    error = True
-                matches = re.findall(r'add{(.*?)}', line)
-                for match in matches:
-                    s.append(match)
-                if line == "remove":
-                    s.pop(0)
-                if line == "get":
-                    print(s[0])
-                    s.pop(0)
-                if line == "show stack":
-                    print(s)
+                for match in matches: print(match)
+                if line == "is": s.append(input("string:"))
+                if line == "ns": s.append(int(input("number:")))
+                if line == "": print(f"S1456\nCAN YOU. I DON'T KNOW. USE COMMANDS IN THE LINE\nERROR AT LINE {ln}");error = True
+                matches = re.findall(r'add{(.*?)}',line)
+                for match in matches: s.append(match)
+                if line == "remove": s.pop(0)
+                if line == "get": print(s.pop(0))
                 if line == "+":
                     r = int(s[0]) + int(s[1])
                     s.append(r)
@@ -61,16 +46,12 @@ while True:
                     s.append(r)
                     s.pop(0)
                     s.pop(0)
-                if line == "duplicate":
-                    s.append(s[0])
+                if line == "duplicate": s.append(s[0])
                 if line == "store":
                     ss.append(s[0])
                     s.pop(0)
                 if line == "unstore":
                     s.append(ss[0])
                     ss.pop(0)
-            else:
-                print("S1245\nP: IS MISSING\nERROR AT LINE 1")
-                error=True
-            if error == True:
-                break
+            else: print("S1245\nP: IS MISSING\nERROR AT LINE 1");error=True
+            if error == True:break
