4a3baff2c56bae1d68574c04fc5319ce5c1ee259
Gaham <glebsemenov1mail.ru@gmail.com>
2024-12-29 23:20:00 +0300

    Update fme.py

--- Rewritten 4me/fme.py
+++ Rewritten 4me/fme.py
@@ -1,10 +1,10 @@
 import os;print("{R4ME}")
 var={};outqm=0;f=input("Input a filename inside the interpriters folder:")
-p=0;stk=[];val=0
+stk=[]
 dir=os.path.realpath(__file__);dir2=os.path.dirname(dir)
 a=[]
 print("\nOutput:")
-if "s"in a:o=open(f"{f}.4")
+if"s"in a:o=open(f"{f}.4")
 else:o=open(f"{dir2}\\{f}.4")
 l=o.readlines();l.append("\n");l.append(str(l.pop(len(l)-2))+str(l.pop()))
 while l:
@@ -22,4 +22,4 @@ while l:
     else:print("Your line contians an undefined command",l)
     l.pop(0)
 if outqm==0:print("No output has been found")
-if "d"in a:print(f,var)
+if"d"in a:print(f,var)
