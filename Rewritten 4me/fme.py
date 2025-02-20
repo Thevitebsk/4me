98374f9c411b7abedd27f794cbf4757e147f2954
Gaham <glebsemenov1mail.ru@gmail.com>
2025-02-20 11:10:01 +0300

    Update fme.py

--- Rewritten 4me/fme.py
+++ Rewritten 4me/fme.py
@@ -1,18 +1,14 @@
 a=[]
 import os;print("{R4ME}")
-var={};stk=[];outqm=0;f=input("Input a filename inside the interpriters folder:")
+var={};outqm=0;f=input("Input a filename inside the interpriters folder:");print("\nOutput:")
 dir=os.path.realpath(__file__);dir2=os.path.dirname(dir)
-print("\nOutput:")
-if"s"in a:o=open(f"{f}.4")
-else:o=open(f"{dir2}\\{f}.4")
-l=o.readlines();l.append("\n");l.append(str(l.pop(len(l)-2))+str(l.pop()))
+if"s"in a:o=open(f"{f}")
+else:o=open(f"{dir2}\\{f}")
+l=o.readlines();l.append(l.pop()+"\n")
 while l:
     s=l[0].split("{")
     if "d"in a:print(s)
-    if s[0]=="echo":
-        if s[1].replace("}\n","")=="s":print(stk.pop())
-        else:print(s[1].replace("}\n",""))
-        outqm=1
+    if s[0]=="echo":print(s[1].replace("}\n",""));outqm=1
     elif s[0]=="var":
         f=s[1].split("=")
         if f[1]=="user}\n":var[f[0]]=input()
@@ -21,8 +17,7 @@ while l:
         s.pop(0);outqm=1
         if s[0].replace("}\n","") in var:print(var[s[0].replace("}\n","")])
     elif l[0][0]=="#":...
-    elif s[0]=="push":stk.append(s[1].replace("}\n",""))
-    else:print("Your line contians an undefined command",l)
+    else:print("Your line contians an undefined command!",l)
     l.pop(0)
 if outqm==0:print("No output has been found")
 if"d"in a:print(var,outqm)
